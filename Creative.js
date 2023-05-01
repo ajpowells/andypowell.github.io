9e0ec51a2cbc48f8523c50d6f4903aec99922292
ajpowells <108973131+ajpowells@users.noreply.github.com>
2023-05-01 19:49:52 -0400

    Update Creative.js

--- Creative.js
+++ Creative.js
@@ -1,4 +1,4 @@
-//alert("Welcome To My Page");
+//alert("Welcome To My Skiing Page");
 //function ItisaFunction() {
 	//document.open();
 	//document.write("<p>To Learn More, Please Feel Free To Contact Me</p>");
@@ -9,24 +9,23 @@
 function WhatYearFunction() {
 	var ByYear = document.getElementById("myInput").value;
 	var text;
-	if (ByYear <= 11) {
+	if (ByYear <= 4) {
 			text= "Close enough, Try Again";
 	}
-	else if (ByYear <= 12) {
+	else if (ByYear <= 6) {
 		text="You got it";
 	}
 	else {
-		text="Please enter an integer 0-12";
+		text="Please enter an integer 0-6";
 	}
 	document.getElementById("Years").innerHTML = text;
 	//document.getElementById("Time").style.display = "none";
 	
 }
 function MoreInformation () {
-	var Information = window.open("https://www.hplct.org/assets/uploads/files/backgrounder_bhutanese.pdf", "MessageWindow", "width = 500, height=100");
+	var Information = window.open("https://www.skimag.com/ski-resort-life/top-30-resorts-of-2019/", "MessageWindow", "width = 500, height=100");
 	Information.document.write("Click Again To Access The Page");
 
 
 	
 }
-
